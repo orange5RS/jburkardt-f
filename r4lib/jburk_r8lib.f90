f2d3822aa3c4e1ce970606b423a5bb9646f5d93d
orange5RS <57889664+orange5RS@users.noreply.github.com>
2019-11-19 18:50:40 +0900

    Create jburk_r8lib.f90

---
+++ r4lib/jburk_r8lib.f90
@@ -0,0 +1,8 @@
+module     jburk_r4lib
+       use jburk_r4_
+       use jburk_r4vec_
+       use jburk_r4col_
+       use jburk_r4row_
+       use jburk_r4mat_
+       use jburk_r4poly_
+end module jburk_r4lib
