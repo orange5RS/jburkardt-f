122057a8e036fb30a6186f9a1658dc8aa2be3401
orange5RS <orange5rs@users.noreply.github.com>
2020-01-19 16:25:45 +0900

    Update jburk_r8lib.f90

--- r8lib/jburk_r8lib.f90
+++ r8lib/jburk_r8lib.f90
@@ -1,8 +1,9 @@
 module     jburk_r8lib
 implicit none
-       use jburk_r8_
-       use jburk_r8col_
-       use jburk_r8row_
-       use jburk_r8vec_
-       use jburk_r8mat_
+   use     jburk_r8_
+   use     jburk_r8poly_
+   use     jburk_r8vec_
+   use     jburk_r8mat_
+   use     jburk_r8col_
+   use     jburk_r8row_
 end module jburk_r8lib
