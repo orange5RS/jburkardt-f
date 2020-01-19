74572c4994cd8b18c41eee4521c7086a21a6ae08
orange5RS <orange5rs@users.noreply.github.com>
2020-01-19 15:07:14 +0900

    Create jburk_r8lib.f90

---
+++ r8lib/jburk_r8lib.f90
@@ -0,0 +1,8 @@
+module     jburk_r8lib
+implicit none
+       use jburk_r8_
+       use jburk_r8col_
+       use jburk_r8row_
+       use jburk_r8vec_
+       use jburk_r8mat_
+end module jburk_r8lib
