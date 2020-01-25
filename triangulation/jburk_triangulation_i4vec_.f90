3184907c5da1ae3b6ad3eab8865c1d00b7140e84
orange5RS <orange5rs@users.noreply.github.com>
2020-01-26 00:39:06 +0900

    Update jburk_triangulation_i4vec_.f90

--- triangulation/jburk_triangulation_i4vec_.f90
+++ triangulation/jburk_triangulation_i4vec_.f90
@@ -43,6 +43,21 @@ implicit none
    end interface    i4vec2_sorted_unique
    public           i4vec2_sorted_unique
 
+   interface        perm_check2
+   module procedure perm_check2
+   end interface    perm_check2
+   public           perm_check2
+
+   interface        perm_inverse
+   module procedure perm_inverse
+   end interface    perm_inverse
+   public           perm_inverse
+
+   interface        sort_heap_external
+   module procedure sort_heap_external
+   end interface    sort_heap_external
+   public           sort_heap_external
+
 contains
 
 
@@ -721,6 +736,403 @@ subroutine i4vec2_sorted_unique ( n, a1, a2, unique_num )
 
   return
 end
+
+
+subroutine perm_check2 ( n, p, base, ierror )
+
+!*****************************************************************************80
+!
+!! PERM_CHECK2 checks that a vector represents a permutation.
+!
+!  Discussion:
+!
+!    The routine verifies that each of the integers from BASE to
+!    to BASE+N-1 occurs among the N entries of the permutation.
+!
+!    Set the input quantity BASE to 0, if P is a 0-based permutation,
+!    or to 1 if P is a 1-based permutation.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    31 October 2008
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of entries.
+!
+!    Input, integer ( kind = 4 ) P(N), the array to check.
+!
+!    Input, integer ( kind = 4 ) BASE, the index base.
+!
+!    Output, integer ( kind = 4 ) IERROR, error flag.
+!    0, the array represents a permutation.
+!    nonzero, the array does not represent a permutation.  The smallest
+!    missing value is equal to IERROR.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) base
+  integer ( kind = 4 ) find
+  integer ( kind = 4 ) ierror
+  integer ( kind = 4 ) p(n)
+  integer ( kind = 4 ) seek
+
+  ierror = 0
+
+  do seek = base, base + n - 1
+
+    ierror = 1
+
+    do find = 1, n
+      if ( p(find) == seek ) then
+        ierror = 0
+        exit
+      end if
+    end do
+
+    if ( ierror /= 0 ) then
+      write ( *, '(a)' ) ' '
+      write ( *, '(a)' ) 'PERM_CHECK2 - Fatal error!'
+      write ( *, '(a)' ) '  The input array does not represent'
+      write ( *, '(a)' ) '  a proper permutation.'
+      stop
+    end if
+
+  end do
+
+  return
+end
+
+
+
+subroutine perm_inverse ( n, p )
+
+!*****************************************************************************80
+!
+!! PERM_INVERSE inverts a permutation "in place".
+!
+!  Discussion:
+!
+!    This algorithm assumes that the entries in the permutation vector are
+!    strictly positive.  In particular, the value 0 must not occur.
+!
+!    When necessary, this function shifts the data temporarily so that
+!    this requirement is satisfied.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    03 June 2009
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of objects being permuted.
+!
+!    Input/output, integer ( kind = 4 ) P(N), the permutation, in standard
+!    index form.  On output, P describes the inverse permutation
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) base
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) i0
+  integer ( kind = 4 ) i1
+  integer ( kind = 4 ) i2
+  integer ( kind = 4 ) i4_sign
+  integer ( kind = 4 ) ierror
+  integer ( kind = 4 ) is
+  integer ( kind = 4 ) p(n)
+  integer ( kind = 4 ) p_min
+
+  if ( n <= 0 ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'PERM_INVERSE - Fatal error!'
+    write ( *, '(a,i8)' ) '  Input value of N = ', n
+    stop
+  end if
+!
+!  Find the least value, and shift data so it begins at 1.
+!
+  call i4vec_min ( n, p, p_min )
+  base = 1
+  p(1:n) = p(1:n) - p_min + base
+!
+!  Check the permutation.
+!
+  call perm_check2 ( n, p, base, ierror )
+
+  if ( ierror /= 0 ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'PERM_INVERSE - Fatal error!'
+    write ( *, '(a)' ) '  PERM_CHECK rejects this permutation.'
+    stop
+  end if
+!
+!  Invert the permutation.
+!
+  is = 1
+
+  do i = 1, n
+
+    i1 = p(i)
+
+    do while ( i < i1 )
+      i2 = p(i1)
+      p(i1) = -i2
+      i1 = i2
+    end do
+
+    is = - i4_sign ( p(i) )
+    p(i) = is * abs ( p(i) )
+
+  end do
+
+  do i = 1, n
+
+    i1 = - p(i)
+
+    if ( 0 <= i1 ) then
+
+      i0 = i
+
+      do
+
+        i2 = p(i1)
+        p(i1) = i0
+
+        if ( i2 < 0 ) then
+          exit
+        end if
+
+        i0 = i1
+        i1 = i2
+
+      end do
+
+    end if
+
+  end do
+!
+!  Reverse the shift.
+!
+  p(1:n) = p(1:n) + p_min - base
+
+  return
+end
+
+
+
+subroutine sort_heap_external ( n, indx, i, j, isgn )
+
+!*****************************************************************************80
+!
+!! SORT_HEAP_EXTERNAL externally sorts a list of items into ascending order.
+!
+!  Discussion:
+!
+!    The actual list of data is not passed to the routine.  Hence this
+!    routine may be used to sort integers, real ( kind = 8 )s, numbers, names,
+!    dates, shoe sizes, and so on.  After each call, the routine asks
+!    the user to compare or interchange two items, until a special
+!    return value signals that the sorting is completed.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    05 February 2004
+!
+!  Author:
+!
+!    Original FORTRAN77 version by Albert Nijenhuis, Herbert Wilf.
+!    FORTRAN90 version by John Burkardt.
+!
+!  Reference:
+!
+!    Albert Nijenhuis, Herbert Wilf,
+!    Combinatorial Algorithms,
+!    Academic Press, 1978, second edition,
+!    ISBN 0-12-519260-6.
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of items to be sorted.
+!
+!    Input/output, integer ( kind = 4 ) INDX, the main communication signal.
+!
+!    The user must set INDX to 0 before the first call.
+!    Thereafter, the user should not change the value of INDX until
+!    the sorting is done.
+!
+!    On return, if INDX is
+!
+!      greater than 0,
+!      * interchange items I and J;
+!      * call again.
+!
+!      less than 0,
+!      * compare items I and J;
+!      * set ISGN = -1 if I < J, ISGN = +1 if J < I;
+!      * call again.
+!
+!      equal to 0, the sorting is done.
+!
+!    Output, integer ( kind = 4 ) I, J, the indices of two items.
+!    On return with INDX positive, elements I and J should be interchanged.
+!    On return with INDX negative, elements I and J should be compared, and
+!    the result reported in ISGN on the next call.
+!
+!    Input, integer ( kind = 4 ) ISGN, results of comparison of elements I
+!    and J.  (Used only when the previous call returned INDX less than 0).
+!    ISGN <= 0 means I is less than or equal to J;
+!    0 <= ISGN means I is greater than or equal to J.
+!
+  implicit none
+
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ), save :: i_save = 0
+  integer ( kind = 4 ) indx
+  integer ( kind = 4 ) isgn
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ), save :: j_save = 0
+  integer ( kind = 4 ), save :: k = 0
+  integer ( kind = 4 ), save :: k1 = 0
+  integer ( kind = 4 ) n
+  integer ( kind = 4 ), save :: n1 = 0
+!
+!  INDX = 0: This is the first call.
+!
+  if ( indx == 0 ) then
+
+    i_save = 0
+    j_save = 0
+    k = n / 2
+    k1 = k
+    n1 = n
+!
+!  INDX < 0: The user is returning the results of a comparison.
+!
+  else if ( indx < 0 ) then
+
+    if ( indx == -2 ) then
+
+      if ( isgn < 0 ) then
+        i_save = i_save + 1
+      end if
+
+      j_save = k1
+      k1 = i_save
+      indx = -1
+      i = i_save
+      j = j_save
+      return
+
+    end if
+
+    if ( 0 < isgn ) then
+      indx = 2
+      i = i_save
+      j = j_save
+      return
+    end if
+
+    if ( k <= 1 ) then
+
+      if ( n1 == 1 ) then
+        i_save = 0
+        j_save = 0
+        indx = 0
+      else
+        i_save = n1
+        n1 = n1 - 1
+        j_save = 1
+        indx = 1
+      end if
+
+      i = i_save
+      j = j_save
+      return
+
+    end if
+
+    k = k - 1
+    k1 = k
+!
+!  0 < INDX, the user was asked to make an interchange.
+!
+  else if ( indx == 1 ) then
+
+    k1 = k
+
+  end if
+
+  do
+
+    i_save = 2 * k1
+
+    if ( i_save == n1 ) then
+      j_save = k1
+      k1 = i_save
+      indx = -1
+      i = i_save
+      j = j_save
+      return
+    else if ( i_save <= n1 ) then
+      j_save = i_save + 1
+      indx = -2
+      i = i_save
+      j = j_save
+      return
+    end if
+
+    if ( k <= 1 ) then
+      exit
+    end if
+
+    k = k - 1
+    k1 = k
+
+  end do
+
+  if ( n1 == 1 ) then
+    i_save = 0
+    j_save = 0
+    indx = 0
+    i = i_save
+    j = j_save
+  else
+    i_save = n1
+    n1 = n1 - 1
+    j_save = 1
+    indx = 1
+    i = i_save
+    j = j_save
+  end if
+
+  return
+end
+
 
 
 end module jburk_triangulation_i4vec_
