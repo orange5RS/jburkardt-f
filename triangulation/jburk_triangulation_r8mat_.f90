f34856a122ba312d91a1750e9adfbe5a0d4ec077
orange5RS <orange5rs@users.noreply.github.com>
2020-01-25 21:14:43 +0900

    Create jburk_triangulation_r8mat_.f90

---
+++ triangulation/jburk_triangulation_r8mat_.f90
@@ -0,0 +1,411 @@
+
+
+
+module     jburk_triangulation_r8mat_
+implicit none
+
+   interface        r8mat_print
+   module procedure r8mat_print
+   end interface    r8mat_print
+   public           r8mat_print
+
+   interface        r8mat_print_some
+   module procedure r8mat_print_some
+   end interface    r8mat_print_some
+   public           r8mat_print_some
+
+   interface        r8mat_transpose_print
+   module procedure r8mat_transpose_print
+   end interface    r8mat_transpose_print
+   public           r8mat_transpose_print
+
+   interface        r8mat_transpose_print_some
+   module procedure r8mat_transpose_print_some
+   end interface    r8mat_transpose_print_some
+   public           r8mat_transpose_print_some
+
+   interface        r8mat_uniform_01
+   module procedure r8mat_uniform_01
+   end interface    r8mat_uniform_01
+   public           r8mat_uniform_01
+
+contains
+
+
+
+subroutine r8mat_print ( m, n, a, title )
+
+!*****************************************************************************80
+!
+!! R8MAT_PRINT prints an R8MAT.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    20 May 2004
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, the number of rows in A.
+!
+!    Input, integer ( kind = 4 ) N, the number of columns in A.
+!
+!    Input, real ( kind = 8 ) A(M,N), the matrix.
+!
+!    Input, character ( len = * ) TITLE, a title.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(m,n)
+  character ( len = * ) title
+
+  call r8mat_print_some ( m, n, a, 1, 1, m, n, title )
+
+  return
+end
+subroutine r8mat_print_some ( m, n, a, ilo, jlo, ihi, jhi, title )
+
+!*****************************************************************************80
+!
+!! R8MAT_PRINT_SOME prints some of an R8MAT.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    26 March 2005
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns.
+!
+!    Input, real ( kind = 8 ) A(M,N), an M by N matrix to be printed.
+!
+!    Input, integer ( kind = 4 ) ILO, JLO, the first row and column to print.
+!
+!    Input, integer ( kind = 4 ) IHI, JHI, the last row and column to print.
+!
+!    Input, character ( len = * ) TITLE, a title.
+!
+  implicit none
+
+  integer ( kind = 4 ), parameter :: incx = 5
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(m,n)
+  character ( len = 14 ) ctemp(incx)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) i2hi
+  integer ( kind = 4 ) i2lo
+  integer ( kind = 4 ) ihi
+  integer ( kind = 4 ) ilo
+  integer ( kind = 4 ) inc
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) j2
+  integer ( kind = 4 ) j2hi
+  integer ( kind = 4 ) j2lo
+  integer ( kind = 4 ) jhi
+  integer ( kind = 4 ) jlo
+  character ( len = * )  title
+
+  write ( *, '(a)' ) ' '
+  write ( *, '(a)' ) trim ( title )
+
+  do j2lo = max ( jlo, 1 ), min ( jhi, n ), incx
+
+    j2hi = j2lo + incx - 1
+    j2hi = min ( j2hi, n )
+    j2hi = min ( j2hi, jhi )
+
+    inc = j2hi + 1 - j2lo
+
+    write ( *, '(a)' ) ' '
+
+    do j = j2lo, j2hi
+      j2 = j + 1 - j2lo
+      write ( ctemp(j2), '(i7,7x)') j
+    end do
+
+    write ( *, '(''  Col   '',5a14)' ) ctemp(1:inc)
+    write ( *, '(a)' ) '  Row'
+    write ( *, '(a)' ) ' '
+
+    i2lo = max ( ilo, 1 )
+    i2hi = min ( ihi, m )
+
+    do i = i2lo, i2hi
+
+      do j2 = 1, inc
+
+        j = j2lo - 1 + j2
+
+        if ( a(i,j) == real ( int ( a(i,j) ), kind = 8 ) ) then
+          write ( ctemp(j2), '(f8.0,6x)' ) a(i,j)
+        else
+          write ( ctemp(j2), '(g14.6)' ) a(i,j)
+        end if
+
+      end do
+
+      write ( *, '(i5,1x,5a14)' ) i, ( ctemp(j), j = 1, inc )
+
+    end do
+
+  end do
+
+  return
+end
+
+subroutine r8mat_transpose_print ( m, n, a, title )
+
+!*****************************************************************************80
+!
+!! R8MAT_TRANSPOSE_PRINT prints an R8MAT, transposed.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    14 June 2004
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns.
+!
+!    Input, real ( kind = 8 ) A(M,N), an M by N matrix to be printed.
+!
+!    Input, character ( len = * ) TITLE, an optional title.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(m,n)
+  character ( len = * ) title
+
+  call r8mat_transpose_print_some ( m, n, a, 1, 1, m, n, title )
+
+  return
+end
+
+
+subroutine r8mat_transpose_print_some ( m, n, a, ilo, jlo, ihi, jhi, title )
+
+!*****************************************************************************80
+!
+!! R8MAT_TRANSPOSE_PRINT_SOME prints some of an R8MAT, transposed.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    14 June 2004
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns.
+!
+!    Input, real ( kind = 8 ) A(M,N), an M by N matrix to be printed.
+!
+!    Input, integer ( kind = 4 ) ILO, JLO, the first row and column to print.
+!
+!    Input, integer ( kind = 4 ) IHI, JHI, the last row and column to print.
+!
+!    Input, character ( len = * ) TITLE, a title.
+!
+  implicit none
+
+  integer ( kind = 4 ), parameter :: incx = 5
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  real ( kind = 8 ) a(m,n)
+  character ( len = 14 ) ctemp(incx)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) i2
+  integer ( kind = 4 ) i2hi
+  integer ( kind = 4 ) i2lo
+  integer ( kind = 4 ) ihi
+  integer ( kind = 4 ) ilo
+  integer ( kind = 4 ) inc
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) j2hi
+  integer ( kind = 4 ) j2lo
+  integer ( kind = 4 ) jhi
+  integer ( kind = 4 ) jlo
+  character ( len = * )  title
+
+  write ( *, '(a)' ) ' '
+  write ( *, '(a)' ) trim ( title )
+
+  do i2lo = max ( ilo, 1 ), min ( ihi, m ), incx
+
+    i2hi = i2lo + incx - 1
+    i2hi = min ( i2hi, m )
+    i2hi = min ( i2hi, ihi )
+
+    inc = i2hi + 1 - i2lo
+
+    write ( *, '(a)' ) ' '
+
+    do i = i2lo, i2hi
+      i2 = i + 1 - i2lo
+      write ( ctemp(i2), '(i7,7x)') i
+    end do
+
+    write ( *, '(''  Row   '',5a14)' ) ctemp(1:inc)
+    write ( *, '(a)' ) '  Col'
+
+    j2lo = max ( jlo, 1 )
+    j2hi = min ( jhi, n )
+
+    do j = j2lo, j2hi
+
+      do i2 = 1, inc
+        i = i2lo - 1 + i2
+        write ( ctemp(i2), '(g14.6)' ) a(i,j)
+      end do
+
+      write ( *, '(i5,1x,5a14)' ) j, ( ctemp(i), i = 1, inc )
+
+    end do
+
+  end do
+
+  return
+end
+
+
+subroutine r8mat_uniform_01 ( m, n, seed, r )
+
+!*****************************************************************************80
+!
+!! R8MAT_UNIFORM_01 returns a unit pseudorandom R8MAT.
+!
+!  Discussion:
+!
+!    An R8MAT is an array of R8's.
+!
+!    For now, the input quantity SEED is an integer variable.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    31 May 2007
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Reference:
+!
+!    Paul Bratley, Bennett Fox, Linus Schrage,
+!    A Guide to Simulation,
+!    Second Edition,
+!    Springer, 1987,
+!    ISBN: 0387964673,
+!    LC: QA76.9.C65.B73.
+!
+!    Bennett Fox,
+!    Algorithm 647:
+!    Implementation and Relative Efficiency of Quasirandom
+!    Sequence Generators,
+!    ACM Transactions on Mathematical Software,
+!    Volume 12, Number 4, December 1986, pages 362-376.
+!
+!    Pierre L'Ecuyer,
+!    Random Number Generation,
+!    in Handbook of Simulation,
+!    edited by Jerry Banks,
+!    Wiley, 1998,
+!    ISBN: 0471134031,
+!    LC: T57.62.H37.
+!
+!    Peter Lewis, Allen Goodman, James Miller,
+!    A Pseudo-Random Number Generator for the System/360,
+!    IBM Systems Journal,
+!    Volume 8, Number 2, 1969, pages 136-143.
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns
+!    in the array.
+!
+!    Input/output, integer ( kind = 4 ) SEED, the "seed" value, which
+!    should NOT be 0.  On output, SEED has been updated.
+!
+!    Output, real ( kind = 8 ) R(M,N), the array of pseudorandom values.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ), parameter :: i4_huge = 2147483647
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) k
+  integer ( kind = 4 ) seed
+  real ( kind = 8 ) r(m,n)
+
+  if ( seed == 0 ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'R8MAT_UNIFORM_01 - Fatal error!'
+    write ( *, '(a)' ) '  Input value of SEED = 0.'
+    stop
+  end if
+
+  do j = 1, n
+
+    do i = 1, m
+
+      k = seed / 127773
+
+      seed = 16807 * ( seed - k * 127773 ) - k * 2836
+
+      if ( seed < 0 ) then
+        seed = seed + i4_huge
+      end if
+
+      r(i,j) = real ( seed, kind = 8 ) * 4.656612875D-10
+
+    end do
+  end do
+
+  return
+end
+
+end module jburk_triangulation_r8mat_
