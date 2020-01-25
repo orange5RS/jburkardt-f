7b0f83b20662593296b02d916a4e659d13df28d9
orange5RS <orange5rs@users.noreply.github.com>
2020-01-25 17:04:05 +0900

    Create jburk_triangulation_i4mat_.f90

---
+++ triangulation/jburk_triangulation_i4mat_.f90
@@ -0,0 +1,524 @@
+
+
+module  jburk_triangulation_i4mat_
+implicit none
+
+
+interface        i4mat_transpose_print
+module procedure i4mat_transpose_print
+end interface    i4mat_transpose_print
+public           i4mat_transpose_print
+
+interface        i4mat_transpose_print_some
+module procedure i4mat_transpose_print_some
+end interface    i4mat_transpose_print_some
+public           i4mat_transpose_print_some
+
+interface        i4mat_col_compare
+module procedure i4col_compare
+end interface    i4mat_col_compare
+public           i4mat_col_compare
+
+interface        i4mat_col_sort_a
+module procedure i4col_sort_a
+end interface    i4mat_col_sort_a
+public           i4mat_col_sort_a
+
+interface        i4mat_col_sorted_unique_count
+module procedure i4col_sorted_unique_count
+end interface    i4mat_col_sorted_unique_count
+public           i4mat_col_sorted_unique_count
+
+interface        i4mat_col_swap
+module procedure i4col_swap
+end interface    i4mat_col_swap
+public           i4mat_col_swap
+
+
+
+
+contains
+
+
+subroutine i4mat_transpose_print ( m, n, a, title )
+
+!*****************************************************************************80
+!
+!! I4MAT_TRANSPOSE_PRINT prints an I4MAT, transposed.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    28 December 2004
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns.
+!
+!    Input, integer ( kind = 4 ) A(M,N), an M by N matrix to be printed.
+!
+!    Input, character ( len = * ) TITLE, a title.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) a(m,n)
+  character ( len = * ) title
+
+  call i4mat_transpose_print_some ( m, n, a, 1, 1, m, n, title )
+
+  return
+end
+
+
+
+subroutine i4mat_transpose_print_some ( m, n, a, ilo, jlo, ihi, jhi, title )
+
+!*****************************************************************************80
+!
+!! I4MAT_TRANSPOSE_PRINT_SOME prints some of the transpose of an I4MAT.
+!
+!  Modified:
+!
+!    09 February 2005
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns.
+!
+!    Input, integer ( kind = 4 ) A(M,N), an M by N matrix to be printed.
+!
+!    Input, integer ( kind = 4 ) ILO, JLO, the first row and column to print.
+!
+!    Input, integer ( kind = 4 ) IHI, JHI, the last row and column to print.
+!
+!    Input, character ( len = * ) TITLE, a title.
+!
+  implicit none
+
+  integer ( kind = 4 ), parameter :: incx = 10
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) a(m,n)
+  character ( len = 7 ) ctemp(incx)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) i2
+  integer ( kind = 4 ) i2hi
+  integer ( kind = 4 ) i2lo
+  integer ( kind = 4 ) ihi
+  integer ( kind = 4 ) ilo
+  integer ( kind = 4 ) inc
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) j2hi
+  integer ( kind = 4 ) j2lo
+  integer ( kind = 4 ) jhi
+  integer ( kind = 4 ) jlo
+  character ( len = * ) title
+
+  write ( *, '(a)' ) ' '
+  write ( *, '(a)' ) trim ( title )
+
+  do i2lo = max ( ilo, 1 ), min ( ihi, m ), incx
+
+    i2hi = i2lo + incx - 1
+    i2hi = min ( i2hi, m )
+    i2hi = min ( i2hi, ihi )
+
+    inc = i2hi + 1 - i2lo
+
+    write ( *, '(a)' ) ' '
+
+    do i = i2lo, i2hi
+      i2 = i + 1 - i2lo
+      write ( ctemp(i2), '(i7)') i
+    end do
+
+    write ( *, '(''  Row '',10a7)' ) ctemp(1:inc)
+    write ( *, '(a)' ) '  Col'
+    write ( *, '(a)' ) ' '
+
+    j2lo = max ( jlo, 1 )
+    j2hi = min ( jhi, n )
+
+    do j = j2lo, j2hi
+
+      do i2 = 1, inc
+
+        i = i2lo - 1 + i2
+
+        write ( ctemp(i2), '(i7)' ) a(i,j)
+
+      end do
+
+      write ( *, '(i5,1x,10a7)' ) j, ( ctemp(i), i = 1, inc )
+
+    end do
+
+  end do
+
+  return
+end
+
+
+
+
+subroutine i4col_compare ( m, n, a, i, j, isgn )
+
+!*****************************************************************************80
+!
+!! I4COL_COMPARE compares columns I and J of an I4COL.
+!
+!  Example:
+!
+!    Input:
+!
+!      M = 3, N = 4, I = 2, J = 4
+!
+!      A = (
+!        1  2  3  4
+!        5  6  7  8
+!        9 10 11 12 )
+!
+!    Output:
+!
+!      ISGN = -1
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    30 June 2000
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns.
+!
+!    Input, integer ( kind = 4 ) A(M,N), an array of N columns of vectors
+!    of length M.
+!
+!    Input, integer ( kind = 4 ) I, J, the columns to be compared.
+!    I and J must be between 1 and N.
+!
+!    Output, integer ( kind = 4 ) ISGN, the results of the comparison:
+!    -1, column I < column J,
+!     0, column I = column J,
+!    +1, column J < column I.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) a(m,n)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) isgn
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) k
+!
+!  Check.
+!
+  if ( i < 1 .or. n < i ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'I4COL_COMPARE - Fatal error!'
+    write ( *, '(a)' ) '  Column index I is out of bounds.'
+    stop
+  end if
+
+  if ( j < 1 .or. n < j ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'I4COL_COMPARE - Fatal error!'
+    write ( *, '(a)' ) '  Column index J is out of bounds.'
+    stop
+  end if
+
+  isgn = 0
+
+  if ( i == j ) then
+    return
+  end if
+
+  k = 1
+
+  do while ( k <= m )
+
+    if ( a(k,i) < a(k,j) ) then
+      isgn = -1
+      return
+    else if ( a(k,j) < a(k,i) ) then
+      isgn = +1
+      return
+    end if
+
+    k = k + 1
+
+  end do
+
+  return
+end
+
+
+
+subroutine i4col_sort_a ( m, n, a )
+
+!*****************************************************************************80
+!
+!! I4COL_SORT_A ascending sorts an I4COL.
+!
+!  Discussion:
+!
+!    In lexicographic order, the statement "X < Y", applied to two real
+!    vectors X and Y of length M, means that there is some index I, with
+!    1 <= I <= M, with the property that
+!
+!      X(J) = Y(J) for J < I,
+!    and
+!      X(I) < Y(I).
+!
+!    In other words, the first time they differ, X is smaller.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    25 September 2001
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, the number of rows of A, and the length of
+!    a vector of data.
+!
+!    Input, integer ( kind = 4 ) N, the number of columns of A.
+!
+!    Input/output, integer ( kind = 4 ) A(M,N).
+!    On input, the array of N columns of M-vectors.
+!    On output, the columns of A have been sorted in ascending
+!    lexicographic order.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) a(m,n)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) indx
+  integer ( kind = 4 ) isgn
+  integer ( kind = 4 ) j
+
+  if ( m <= 0 ) then
+    return
+  end if
+
+  if ( n <= 1 ) then
+    return
+  end if
+!
+!  Initialize.
+!
+  i = 0
+  indx = 0
+  isgn = 0
+  j = 0
+!
+!  Call the external heap sorter.
+!
+  do
+
+    call sort_heap_external ( n, indx, i, j, isgn )
+!
+!  Interchange the I and J objects.
+!
+    if ( 0 < indx ) then
+
+      call i4col_swap ( m, n, a, i, j )
+!
+!  Compare the I and J objects.
+!
+    else if ( indx < 0 ) then
+
+      call i4col_compare ( m, n, a, i, j, isgn )
+
+    else if ( indx == 0 ) then
+
+      exit
+
+    end if
+
+  end do
+
+  return
+end
+
+
+
+subroutine i4col_sorted_unique_count ( m, n, a, unique_num )
+
+!*****************************************************************************80
+!
+!! I4COL_SORTED_UNIQUE_COUNT counts unique elements in an I4COL.
+!
+!  Discussion:
+!
+!    The columns of the array may be ascending or descending sorted.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    17 February 2005
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns.
+!
+!    Input, integer ( kind = 4 ) A(M,N), a sorted array, containing
+!    N columns of data.
+!
+!    Output, integer ( kind = 4 ) UNIQUE_NUM, the number of unique columns.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) a(m,n)
+  integer ( kind = 4 ) j1
+  integer ( kind = 4 ) j2
+  integer ( kind = 4 ) unique_num
+
+  if ( n <= 0 ) then
+    unique_num = 0
+    return
+  end if
+
+  unique_num = 1
+  j1 = 1
+
+  do j2 = 2, n
+
+    if ( any ( a(1:m,j1) /= a(1:m,j2) ) ) then
+      unique_num = unique_num + 1
+      j1 = j2
+    end if
+
+  end do
+
+  return
+end
+
+
+
+subroutine i4col_swap ( m, n, a, i, j )
+
+!*****************************************************************************80
+!
+!! I4COL_SWAP swaps columns I and J of an I4COL.
+!
+!  Example:
+!
+!    Input:
+!
+!      M = 3, N = 4, I = 2, J = 4
+!
+!      A = (
+!        1  2  3  4
+!        5  6  7  8
+!        9 10 11 12 )
+!
+!    Output:
+!
+!      A = (
+!        1  4  3  2
+!        5  8  7  6
+!        9 12 11 10 )
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    04 April 2001
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) M, N, the number of rows and columns
+!    in the array.
+!
+!    Input/output, integer ( kind = 4 ) A(M,N), an array of N columns
+!    of length M.
+!
+!    Input, integer ( kind = 4 ) I, J, the columns to be swapped.
+!
+  implicit none
+
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) a(m,n)
+  integer ( kind = 4 ) col(m)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) j
+
+  if ( i < 1 .or. n < i .or. j < 1 .or. n < j ) then
+
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'I4COL_SWAP - Fatal error!'
+    write ( *, '(a)' ) '  I or J is out of bounds.'
+    write ( *, '(a,i8)' ) '  I =    ', i
+    write ( *, '(a,i8)' ) '  J =    ', j
+    write ( *, '(a,i8)' ) '  N =    ', n
+    stop
+
+  end if
+
+  if ( i == j ) then
+    return
+  end if
+
+  col(1:m) = a(1:m,i)
+  a(1:m,i) = a(1:m,j)
+  a(1:m,j) = col(1:m)
+
+  return
+end
+
+
+end module jburk_triangulation_i4mat_
+
