022c36f3131cd252e35961603172f4331b79720d
orange5RS <orange5rs@users.noreply.github.com>
2020-01-25 21:07:45 +0900

    Create jburk_triangulation_r8vec_.f90

---
+++ triangulation/jburk_triangulation_r8vec_.f90
@@ -0,0 +1,455 @@
+
+
+module jburk_triangulation_r8vec_
+implicit none
+
+   interface        r8vec_bracket
+   module procedure r8vec_bracket
+   end interface    r8vec_bracket
+   public           r8vec_bracket
+
+   interface        r8vec_uniform_01
+   module procedure r8vec_uniform_01
+   end interface    r8vec_uniform_01
+   public           r8vec_uniform_01
+
+   interface        r82vec_permute
+   module procedure r82vec_permute
+   end interface    r82vec_permute
+   public           r82vec_permute
+
+   interface        r82vec_sort_heap_index_a
+   module procedure r82vec_sort_heap_index_a
+   end interface    r82vec_sort_heap_index_a
+   public           r82vec_sort_heap_index_a
+
+contains
+
+subroutine r8vec_bracket ( n, x, xval, left, right )
+
+!*****************************************************************************80
+!
+!! R8VEC_BRACKET searches a sorted R8VEC for successive brackets of a value.
+!
+!  Discussion:
+!
+!    If the values in the vector are thought of as defining intervals
+!    on the real line, then this routine searches for the interval
+!    nearest to or containing the given value.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    06 April 1999
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, length of input array.
+!
+!    Input, real ( kind = 8 ) X(N), an array that has been sorted into
+!    ascending order.
+!
+!    Input, real ( kind = 8 ) XVAL, a value to be bracketed.
+!
+!    Output, integer ( kind = 4 ) LEFT, RIGHT, the results of the search.
+!    Either:
+!      XVAL < X(1), when LEFT = 1, RIGHT = 2;
+!      X(N) < XVAL, when LEFT = N-1, RIGHT = N;
+!    or
+!      X(LEFT) <= XVAL <= X(RIGHT).
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) left
+  integer ( kind = 4 ) right
+  real ( kind = 8 ) x(n)
+  real ( kind = 8 ) xval
+
+  do i = 2, n - 1
+
+    if ( xval < x(i) ) then
+      left = i - 1
+      right = i
+      return
+    end if
+
+   end do
+
+  left = n - 1
+  right = n
+
+  return
+end
+
+subroutine r8vec_uniform_01 ( n, seed, r )
+
+!*****************************************************************************80
+!
+!! R8VEC_UNIFORM_01 returns a unit pseudorandom R8VEC.
+!
+!  Discussion:
+!
+!    An R8VEC is a vector of R8's.
+!
+!    For now, the input quantity SEED is an integer variable.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    31 May 2007
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Reference:
+!
+!    Paul Bratley, Bennett Fox, Linus Schrage,
+!    A Guide to Simulation,
+!    Second Edition,
+!    Springer, 1987,
+!    ISBN: 0387964673,
+!    LC: QA76.9.C65.B73.
+!
+!    Bennett Fox,
+!    Algorithm 647:
+!    Implementation and Relative Efficiency of Quasirandom
+!    Sequence Generators,
+!    ACM Transactions on Mathematical Software,
+!    Volume 12, Number 4, December 1986, pages 362-376.
+!
+!    Pierre L'Ecuyer,
+!    Random Number Generation,
+!    in Handbook of Simulation,
+!    edited by Jerry Banks,
+!    Wiley, 1998,
+!    ISBN: 0471134031,
+!    LC: T57.62.H37.
+!
+!    Peter Lewis, Allen Goodman, James Miller,
+!    A Pseudo-Random Number Generator for the System/360,
+!    IBM Systems Journal,
+!    Volume 8, Number 2, 1969, pages 136-143.
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of entries in the vector.
+!
+!    Input/output, integer ( kind = 4 ) SEED, the "seed" value, which
+!    should NOT be 0.  On output, SEED has been updated.
+!
+!    Output, real ( kind = 8 ) R(N), the vector of pseudorandom values.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ), parameter :: i4_huge = 2147483647
+  integer ( kind = 4 ) k
+  integer ( kind = 4 ) seed
+  real ( kind = 8 ) r(n)
+
+  if ( seed == 0 ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'R8VEC_UNIFORM_01 - Fatal error!'
+    write ( *, '(a)' ) '  Input value of SEED = 0.'
+    stop
+  end if
+
+  do i = 1, n
+
+    k = seed / 127773
+
+    seed = 16807 * ( seed - k * 127773 ) - k * 2836
+
+    if ( seed < 0 ) then
+      seed = seed + i4_huge
+    end if
+
+    r(i) = real ( seed, kind = 8 ) * 4.656612875D-10
+
+  end do
+
+  return
+end
+
+
+
+subroutine r82vec_permute ( n, p, a )
+
+!*****************************************************************************80
+!
+!! R82VEC_PERMUTE permutes an R82VEC in place.
+!
+!  Discussion:
+!
+!    An R82VEC is an array of pairs of R8 values.
+!
+!    The same logic can be used to permute an array of objects of any
+!    arithmetic type, or an array of objects of any complexity.  The only
+!    temporary storage required is enough to store a single object.  The number
+!    of data movements made is N + the number of cycles of order 2 or more,
+!    which is never more than N + N/2.
+!
+!  Example:
+!
+!    Input:
+!
+!      N = 5
+!      P = (   2,    4,    5,    1,    3 )
+!      A = ( 1.0,  2.0,  3.0,  4.0,  5.0 )
+!          (11.0, 22.0, 33.0, 44.0, 55.0 )
+!
+!    Output:
+!
+!      A    = (  2.0,  4.0,  5.0,  1.0,  3.0 )
+!             ( 22.0, 44.0, 55.0, 11.0, 33.0 ).
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    13 March 2005
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of objects.
+!
+!    Input, integer ( kind = 4 ) P(N), the permutation.  P(I) = J means
+!    that the I-th element of the output array should be the J-th
+!    element of the input array.
+!
+!    Input/output, real ( kind = 8 ) A(2,N), the array to be permuted.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+  integer ( kind = 4 ), parameter :: dim_num = 2
+
+  real ( kind = 8 ) a(dim_num,n)
+  real ( kind = 8 ) a_temp(dim_num)
+  integer ( kind = 4 ), parameter :: base = 1
+  integer ( kind = 4 ) ierror
+  integer ( kind = 4 ) iget
+  integer ( kind = 4 ) iput
+  integer ( kind = 4 ) istart
+  integer ( kind = 4 ) p(n)
+
+  call perm_check2 ( n, p, base, ierror )
+
+  if ( ierror /= 0 ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'R82VEC_PERMUTE - Fatal error!'
+    write ( *, '(a)' ) '  PERM_CHECK rejects this permutation.'
+    stop
+  end if
+!
+!  Search for the next element of the permutation that has not been used.
+!
+  do istart = 1, n
+
+    if ( p(istart) < 0 ) then
+
+      cycle
+
+    else if ( p(istart) == istart ) then
+
+      p(istart) = - p(istart)
+      cycle
+
+    else
+
+      a_temp(1:dim_num) = a(1:dim_num,istart)
+      iget = istart
+!
+!  Copy the new value into the vacated entry.
+!
+      do
+
+        iput = iget
+        iget = p(iget)
+
+        p(iput) = - p(iput)
+
+        if ( iget < 1 .or. n < iget ) then
+          write ( *, '(a)' ) ' '
+          write ( *, '(a)' ) 'R82VEC_PERMUTE - Fatal error!'
+          write ( *, '(a)' ) '  A permutation index is out of range.'
+          write ( *, '(a,i8,a,i8)' ) '  P(', iput, ') = ', iget
+          stop
+        end if
+
+        if ( iget == istart ) then
+          a(1:dim_num,iput) = a_temp(1:dim_num)
+          exit
+        end if
+
+        a(1:dim_num,iput) = a(1:dim_num,iget)
+
+      end do
+
+    end if
+
+  end do
+!
+!  Restore the signs of the entries.
+!
+  p(1:n) = - p(1:n)
+
+  return
+end
+
+
+
+subroutine r82vec_sort_heap_index_a ( n, a, indx )
+
+!*****************************************************************************80
+!
+!! R82VEC_SORT_HEAP_INDEX_A ascending index heaps an R82VEC.
+!
+!  Discussion:
+!
+!    An R82VEC is an array of R82's.
+!
+!    The sorting is not actually carried out.  Rather an index array is
+!    created which defines the sorting.  This array may be used to sort
+!    or index the array, or to sort or index related arrays keyed on the
+!    original array.
+!
+!    Once the index array is computed, the sorting can be carried out
+!    "implicitly:
+!
+!      A(1:2,INDX(1:N)) is sorted,
+!
+!    or explicitly, by the call
+!
+!      call r82vec_permute ( n, indx, a )
+!
+!    after which A(1:2,I), I = 1 to N is sorted.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    08 December 2004
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of entries in the array.
+!
+!    Input, real ( kind = 8 ) A(2,N), an array to be index-sorted.
+!
+!    Output, integer ( kind = 4 ) INDX(N), the sort index.  The
+!    I-th element of the sorted array is A(1:2,INDX(I)).
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+  integer ( kind = 4 ), parameter :: dim_num = 2
+
+  real ( kind = 8 ) a(dim_num,n)
+  real ( kind = 8 ) aval(dim_num)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) indx(n)
+  integer ( kind = 4 ) indxt
+  integer ( kind = 4 ) ir
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) l
+
+  if ( n < 1 ) then
+    return
+  end if
+
+  do i = 1, n
+    indx(i) = i
+  end do
+
+  if ( n == 1 ) then
+    return
+  end if
+
+  l = n / 2 + 1
+  ir = n
+
+  do
+
+    if ( 1 < l ) then
+
+      l = l - 1
+      indxt = indx(l)
+      aval(1:dim_num) = a(1:dim_num,indxt)
+
+    else
+
+      indxt = indx(ir)
+      aval(1:dim_num) = a(1:dim_num,indxt)
+      indx(ir) = indx(1)
+      ir = ir - 1
+
+      if ( ir == 1 ) then
+        indx(1) = indxt
+        exit
+      end if
+
+    end if
+
+    i = l
+    j = l + l
+
+    do while ( j <= ir )
+
+      if ( j < ir ) then
+        if (   a(1,indx(j)) <  a(1,indx(j+1)) .or. &
+             ( a(1,indx(j)) == a(1,indx(j+1)) .and. &
+               a(2,indx(j)) <  a(2,indx(j+1)) ) ) then
+          j = j + 1
+        end if
+      end if
+
+      if (   aval(1) <  a(1,indx(j)) .or. &
+           ( aval(1) == a(1,indx(j)) .and. &
+             aval(2) <  a(2,indx(j)) ) ) then
+        indx(i) = indx(j)
+        i = j
+        j = j + j
+      else
+        j = ir + 1
+      end if
+
+    end do
+
+    indx(i) = indxt
+
+  end do
+
+  return
+end
+
+
+end module jburk_triangulation_r8vec_
