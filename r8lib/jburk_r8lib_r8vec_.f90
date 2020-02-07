f20ee33242bb159af9b73c8357ae4aab7daa91f0
orange5RS <57889664+orange5RS@users.noreply.github.com>
2020-02-07 15:51:49 +0900

    Update jburk_r8lib_r8vec_.f90

--- r8lib/jburk_r8lib_r8vec_.f90
+++ r8lib/jburk_r8lib_r8vec_.f90
@@ -1,5 +1,9 @@
 
-!> @see https://github.com/Mohid-Water-Modelling-System/Mohid/wiki/Documenting-Fortran-with-Doxygen
+
+!> @author [P. Jang](orange224factory@gmail.com)
+!> @date   2020-02-06
+!> @see    [Documenting Fortran with Doxygen](https://github.com/Mohid-Water-Modelling-System/Mohid/wiki/Documenting-Fortran-with-Doxygen)
+
 module     jburk_r8lib_r8vec_
 use, intrinsic :: iso_fortran_env
 implicit none
