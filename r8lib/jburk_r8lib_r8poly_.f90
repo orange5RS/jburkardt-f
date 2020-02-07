f4d6aada3281ad4bc93ed5779957eb12302d2703
orange5RS <57889664+orange5RS@users.noreply.github.com>
2020-02-07 15:51:43 +0900

    Update jburk_r8lib_r8poly_.f90

--- r8lib/jburk_r8lib_r8poly_.f90
+++ r8lib/jburk_r8lib_r8poly_.f90
@@ -1,4 +1,11 @@
+
+
+!> @author [P. Jang](orange224factory@gmail.com)
+!> @date   2020-02-06
+!> @see    [Documenting Fortran with Doxygen](https://github.com/Mohid-Water-Modelling-System/Mohid/wiki/Documenting-Fortran-with-Doxygen)
+
 module     jburk_r8lib_r8poly_
+use, intrinsic :: iso_fortran_env
 implicit none
 
 interface        r8poly_degree
