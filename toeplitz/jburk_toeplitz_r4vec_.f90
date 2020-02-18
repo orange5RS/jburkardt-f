9b095306525a2b97557b98f97706c6496b967ad3
orange5RS <57889664+orange5RS@users.noreply.github.com>
2020-02-18 18:04:20 +0900

    Create jburk_toeplitz_r4vec_.f90

---
+++ toeplitz/jburk_toeplitz_r4vec_.f90
@@ -0,0 +1,12 @@
+
+
+!> @author [P. Jang](orange224factory@gmail.com)
+!> @date   2020-02-18
+!> @see    [Documenting Fortran with Doxygen](https://github.com/Mohid-Water-Modelling-System/Mohid/wiki/Documenting-Fortran-with-Doxygen)
+module     jburk_toeplitz_r4vec_
+use, intrinsic :: iso_fortran_env
+implicit none
+
+contains
+end module jburk_toeplitz_r4vec_
+
