33e38483a703f08b1e94092f9751e88c334aa832
orange5RS <57889664+orange5RS@users.noreply.github.com>
2020-02-18 18:56:18 +0900

    Update jburk_toeplitz_r4vec_.f90

--- toeplitz/jburk_toeplitz_r4vec_.f90
+++ toeplitz/jburk_toeplitz_r4vec_.f90
@@ -8,5 +8,203 @@ use, intrinsic :: iso_fortran_env
 implicit none
 
 contains
+
+
+subroutine r4vec_indicator ( n, a )
+
+!*****************************************************************************80
+!
+!! R4VEC_INDICATOR sets a real vector to the indicator vector.
+!
+!  Modified:
+!
+!    09 February 2001
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of elements of A.
+!
+!    Output, real ( kind = 4 ) A(N), the array to be initialized.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  real ( kind = 4 ) a(n)
+  integer ( kind = 4 ) i
+
+  do i = 1, n
+    a(i) = real ( i, kind = 4 )
+  end do
+
+  return
+end
+subroutine r4vec_print ( n, a, title )
+
+!*****************************************************************************80
+!
+!! R4VEC_PRINT prints a real vector.
+!
+!  Modified:
+!
+!    16 December 1999
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of components of the vector.
+!
+!    Input, real ( kind = 4 ) A(N), the vector to be printed.
+!
+!    Input, character ( len = * ) TITLE, a title.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  real ( kind = 4 ) a(n)
+  integer ( kind = 4 ) i
+  character ( len = * ) title
+
+  write ( *, '(a)' ) ' '
+  write ( *, '(a)' ) trim ( title )
+  write ( *, '(a)' ) ' '
+  do i = 1, n
+    write ( *, '(i8,g14.6)' ) i, a(i)
+  end do
+
+  return
+end
+subroutine r4vec_print_some ( n, a, max_print, title )
+
+!*****************************************************************************80
+!
+!! R4VEC_PRINT_SOME prints "some" of a real vector.
+!
+!  Discussion:
+!
+!    The user specifies MAX_PRINT, the maximum number of lines to print.
+!
+!    If N, the size of the vector, is no more than MAX_PRINT, then
+!    the entire vector is printed, one entry per line.
+!
+!    Otherwise, if possible, the first MAX_PRINT-2 entries are printed,
+!    followed by a line of periods suggesting an omission,
+!    and the last entry.
+!
+!  Modified:
+!
+!    10 September 1999
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, integer ( kind = 4 ) N, the number of entries of the vector.
+!
+!    Input, real ( kind = 4 ) A(N), the vector to be printed.
+!
+!    Input, integer ( kind = 4 ) MAX_PRINT, the maximum number of lines 
+!    to print.
+!
+!    Input, character ( len = * ) TITLE, an optional title.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  real ( kind = 4 ) a(n)
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) max_print
+  character ( len = * ) title
+
+  if ( max_print <= 0 ) then
+    return
+  end if
+
+  if ( n <= 0 ) then
+    return
+  end if
+
+  if ( 0 < len_trim ( title ) ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) trim ( title )
+    write ( *, '(a)' ) ' '
+  end if
+
+  if ( n <= max_print ) then
+
+    do i = 1, n
+      write ( *, '(i8,2x,g14.6)' ) i, a(i)
+    end do
+
+  else if ( 3 <= max_print ) then
+
+    do i = 1, max_print-2
+      write ( *, '(i8,2x,g14.6)' ) i, a(i)
+    end do
+    write ( *, '(a)' ) '......  ..............'
+    i = n
+    write ( *, '(i8,2x,g14.6)' ) i, a(i)
+
+  else
+
+    do i = 1, max_print - 1
+      write ( *, '(i8,2x,g14.6)' ) i, a(i)
+    end do
+    i = max_print
+    write ( *, '(i8,2x,g14.6,2x,a)' ) i, a(i), '...more entries...'
+
+  end if
+
+  return
+end
+subroutine r4vec_random ( alo, ahi, n, a )
+
+!*****************************************************************************80
+!
+!! R4VEC_RANDOM returns a random real vector in a given range.
+!
+!  Modified:
+!
+!    01 December 2000
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Parameters:
+!
+!    Input, real ( kind = 4 ) ALO, AHI, the range allowed for the entries.
+!
+!    Input, integer ( kind = 4 ) N, the number of entries in the vector.
+!
+!    Output, real A(N), the vector of randomly chosen values.
+!
+  implicit none
+
+  integer ( kind = 4 ) n
+
+  real ( kind = 4 ) a(n)
+  real ( kind = 4 ) ahi
+  real ( kind = 4 ) alo
+
+  call random_number ( harvest = a(1:n) )
+
+  a(1:n) = alo + a(1:n) * ( ahi - alo )
+
+  return
+end
+
+
 end module jburk_toeplitz_r4vec_
 
